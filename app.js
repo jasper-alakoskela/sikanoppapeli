27f45413410467e6c04fc948c3148ed3d1d1f2ab
Jasper Alakoskela <jasper.alakosekla@gmail.com>
2020-11-04 13:58:02 +0200

    siivosin koodia

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 
-let stopBtn = document.getElementById("stopbtn");
 let p1Score = document.getElementById("p1score");
 let p2Score = document.getElementById("p2score");
+let status = document.getElementById("status");
+let player1 = document.getElementById("p1");
+let player2 = document.getElementById("p2");
+
+let players = ["player1", "player2"];
 
 function rollDice() {
     let die1 = document.getElementById("die1")
     let status = document.getElementById("status");
     let d1 = Math.floor(Math.random() * 6) + 1;
     die1.innerHTML = d1;
-    checkWin();
     status.innerHTML = d1;
+}
 
-}
-function checkWin() {
-    while (d1 != 1 || stopBtn != clicked) {
-        rollDice()
-        if (d1 == 1 || stopBtn == clicked) {
-            break;
-        }
-    }
-}

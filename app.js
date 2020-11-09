402ad649584c837c885051dc933aca5660653e7c
Jasper Alakoskela <jasper.alakosekla@gmail.com>
2020-11-09 12:25:47 +0200

    korjailin

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 
 let p1Score = document.getElementById("p1score");
 let p2Score = document.getElementById("p2score");
-let status = document.getElementById("status");
 let player1 = document.getElementById("p1");
 let player2 = document.getElementById("p2");
+let score = 0;
 
 let players = ["player1", "player2"];
 
-function rollDice() {
-    let die1 = document.getElementById("die1")
-    let status = document.getElementById("status");
-    let d1 = Math.floor(Math.random() * 6) + 1;
-    die1.innerHTML = d1;
-    status.innerHTML = d1;
+let d1 = parseInt(Math.floor(Math.random() * 6 + 1));
+
+while (true) {
+    score = + d1;
+    if (d1 == 1) {
+        score == 0;
+        break;
+    }
 }
+
+let die1 = document.getElementById("die1")
+let status = document.getElementById("status");
+die1.innerHTML = d1;
+status.innerHTML = score;
+
 

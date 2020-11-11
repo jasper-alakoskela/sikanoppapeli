37c9c05fa622f34f35d60890e8989fd39bd88a26
Jasper Alakoskela <jasper.alakoskela@itvelhot.local>
2020-11-11 13:43:07 +0200

    nopan heitto koodattu

--- app.js
+++ app.js
@@ -3,23 +3,44 @@ let p1Score = document.getElementById("p1score");
 let p2Score = document.getElementById("p2score");
 let player1 = document.getElementById("p1");
 let player2 = document.getElementById("p2");
+let p1Name = document.getElementById("p1name");
+let p2Name = document.getElementById("p2name");
+
+let players = [{name: p1Name, score: p1Score}, {name: p2Name, score: p2Score}]
+
+let dice = 0;
 let score = 0;
+let turn = 0;
 
-let players = ["player1", "player2"];
+function rollDice(){
+    let dice = document.getElementById("die1");
+    dice = Math.floor(Math.random() * 6) + 1;
+    die.innerHTML = dice;
+    score += dice;
 
-let d1 = parseInt(Math.floor(Math.random() * 6 + 1));
-
-while (true) {
-    score = + d1;
-    if (d1 == 1) {
-        score == 0;
-        break;
+    if (dice == 1) {
+        score = 0;
+        turn = 1;
     }
+
+    update();
+    changeTurn();
 }
 
-let die1 = document.getElementById("die1")
-let status = document.getElementById("status");
-die1.innerHTML = d1;
-status.innerHTML = score;
+function changeTurn() {
+    if (turn == 0) {
+        turn = 0;
+       p1Name.style.color = "red";
+       p2Name.style.color = "#cacaca";
+    }
+    else {
+        turn = 1;
+        p2Name.style.color = "red";
+        p1Name.style.color = "#cacaca";
+    }
+    console.log(turn);
+}
 
-
+function update(){
+    document.getElementById("score").innerHTML = score;
+}

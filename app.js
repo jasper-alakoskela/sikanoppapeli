4bc0459897b1d5d0a3cde955945513be53389969
Jasper Alakoskela <jasper.alakosekla@gmail.com>
2020-11-13 13:28:55 +0200

    pisteiden talletus ja vuorot koodattu.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
-let p1Score = document.getElementById("p1score");
-let p2Score = document.getElementById("p2score");
 let player1 = document.getElementById("p1");
 let player2 = document.getElementById("p2");
 let p1Name = document.getElementById("p1name");
 let p2Name = document.getElementById("p2name");
 
-let players = [{name: p1Name, score: p1Score}, {name: p2Name, score: p2Score}]
+let players = [player1, player2]
 
 let dice = 0;
 let score = 0;
 let turn = 0;
+let player1Score = 0;
+let player2Score = 0;
 
-function rollDice(){
+function rollDice() {
     let dice = document.getElementById("die1");
     dice = Math.floor(Math.random() * 6) + 1;
     die.innerHTML = dice;
@@ -20,27 +20,51 @@ function rollDice(){
 
     if (dice == 1) {
         score = 0;
-        turn = 1;
+        changeTurn();
     }
 
     update();
-    changeTurn();
+
+    console.log(dice);
 }
 
 function changeTurn() {
-    if (turn == 0) {
+
+    let currentPlayer = players[turn]
+
+    turn++;
+
+    if (turn == players.length) {
         turn = 0;
-       p1Name.style.color = "red";
-       p2Name.style.color = "#cacaca";
+        p1Name.style.color = "red";
+        p2Name.style.color = "#cacaca";
     }
     else {
         turn = 1;
         p2Name.style.color = "red";
         p1Name.style.color = "#cacaca";
     }
-    console.log(turn);
+
 }
 
-function update(){
+document.getElementById("getscorebtn").addEventListener("click", getScore);
+
+function getScore() {
+    if (turn == 0) {
+        player1Score += score;
+        document.getElementById("p1score").innerHTML = `Pisteet:${player1Score}`;
+        score = 0;
+        changeTurn();
+    }
+    else {
+        player2Score += score;
+        document.getElementById("p2score").innerHTML = `Pisteet:${player2Score}`;
+        score = 0;
+        changeTurn();
+    }
+}
+
+
+function update() {
     document.getElementById("score").innerHTML = score;
 }
